4486a515cfd5752d1e9e2a697dd24cab0fe11eca
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-24 14:05:54 +0100

    //Creating and rendering tags for gallery items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+//Creating and rendering tags for gallery items
+const galleryItem = galleryItems
+  .map(
+    (item) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+//Setting target on div.gallery and getting url of larger img
